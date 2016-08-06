003ecdeeff9f2eb4630996425f2a7f3a58b13a37
Stephanie Boue <stephanieboue@gmail.com>
2016-08-06 15:32:26 +0200

    quasi final

--- FindYourGift/client/FindYourGift.js
+++ FindYourGift/client/FindYourGift.js
@@ -53,21 +53,6 @@ Template.HomePage.onRendered(function() {
 
 });
 
-Template.BrowsePage.onRendered(function() {
-
-  $("#CarouselProducts").owlCarousel({
- 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds 
-      items : 3,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
- 
-  });
-
-});
-
-
-
 // set up the iron router
 Router.configure({
   layoutTemplate: 'ApplicationLayout'
@@ -85,7 +70,7 @@ Router.route('/', function () {
 Router.route('/browse', function () {
   console.log("you hit / ");
   this.render("navbar", {to:"navbar"});
-  this.render("BrowsePage", {to:"main"}); 
+  this.render("images", {to:"main"}); 
   this.render("footer", {to:"footer"});   
 });
 
@@ -93,8 +78,20 @@ Router.route('/browse', function () {
 Router.route('/offer', function () {
   console.log("you hit / ");
   this.render("navbar", {to:"navbar"});
-  this.render("OfferPage", {to:"main"}); 
+  this.render("offer", {to:"main"}); 
   this.render("footer", {to:"footer"});   
+});
+
+Router.route('/image/:_id', function () {
+  this.render('navbar', {
+    to:"navbar"
+  });
+  this.render('image', {
+    to:"main", 
+    data:function(){
+      return Images.findOne({_id:this.params._id});
+    }
+  });
 });
 
 // 'drawing' page
@@ -122,15 +119,147 @@ Router.route('/help', function () {
 });
 
 
-// Tempalte helpers
+// Template helpers
 
-Template.profilePage.helpers({username:function(){
-if (Meteor.user()){
-  return Meteor.user().username;
-    //return Meteor.user().emails[0].address;
+Template.profilePage.helpers({nickname:function(){
+  if (Meteor.user()){
+    return Meteor.user().profile.Nickname;
+      //return Meteor.user().emails[0].address;
+  }
+  else {
+    return "anonymous internet user";
+  }
+  }
+});
+
+
+Template.images.helpers({
+    images:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});         
+      }
+      else {
+        return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});         
+      }
+    },
+    filtering_images:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        return true;
+      } 
+      else {
+        return false;
+      }
+    },
+    getFilterUser:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        var user = Meteor.users.findOne(
+          {_id:Session.get("userFilter")});
+        return user.username;
+      } 
+      else {
+        return false;
+      }
+    },
+    getUser:function(user_id){
+      var user = Meteor.users.findOne({_id:user_id});
+      if (user){
+        return user.profile.Nickname;
+      }
+      else {
+        return "Anonymous";
+      }
+    }
+    });
+
+
+Template.offer.helpers({
+      images:function(){
+        
+          return Images.find({createdBy:Meteor.user()._id}, {sort:{createdOn: -1, rating:-1}});         
+ 
+      },
+      filtering_images:function(){
+        if (Session.get("userFilter")){// they set a filter!
+          return true;
+        } 
+        else {
+          return false;
+        }
+      },
+      getFilterUser:function(){
+        if (Session.get("userFilter")){// they set a filter!
+          var user = Meteor.users.findOne(
+            {_id:Session.get("userFilter")});
+          return user.username;
+        } 
+        else {
+          return false;
+        }
+      },
+      getUser:function(user_id){
+        var user = Meteor.users.findOne({_id:user_id});
+        if (user){
+          return user.username;
+        }
+        else {
+          return "anon";
+        }
 }
-else {
-  return "anonymous internet user";
-}
+    });
+
+
+Template.images.events({
+    'click .js-image':function(event){
+        $(event.target).css("width", "50px");
+    },  
+    'click .js-rate-image':function(event){
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.data_id;
+      console.log(image_id);
+
+      Images.update({_id:image_id}, 
+                    {$set: {rating:rating}});
+    }, 
+});
+
+Template.offer.events({
+    'click .js-del-image':function(event){
+       var image_id = this._id;
+       console.log(image_id);
+       // use jquery to hide the image component
+       // then remove it at the end of the animation
+       $("#"+image_id).hide('slow', function(){
+        Images.remove({"_id":image_id});
+       })  
+    }, 
+    'click .js-show-image-form':function(event){
+      console.log("should display image add form")
+      $("#image_add_form").modal();
+    }, 
+
+'submit .js-add-image':function(event){
+  var img_src, img_alt, price;
+
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    price = event.target.price.value;
+
+    console.log("src: "+img_src+" alt:"+img_alt+" price:"+price);
+    if (Meteor.user()){
+      Images.insert({
+        img_src:img_src, 
+        img_alt:img_alt, 
+        price:price,
+        createdOn:new Date(),
+        createdBy:Meteor.user()._id
+      });
+  }
+    $("#image_add_form").modal('hide');
+ return false;
 }
 });
+
+
+
+
--- FindYourGift/server/serverSide.js
+++ FindYourGift/server/serverSide.js
@@ -5,3 +5,19 @@ Meteor.methods({
     points.remove({});
   },
 });
+
+Meteor.startup(function(){
+if (Images.find().count() == 0){
+	for (var i=1;i<8;i++){
+		Images.insert(
+			{
+				img_src:"Products/Product"+i+".png",
+				img_alt:"Creation"+i,
+				User:"Stephanie"
+			}
+		);	
+	}// end of for insert images
+	// count the images!
+	console.log("startup.js says: "+Images.find().count());
+}// end of if have no images
+});
